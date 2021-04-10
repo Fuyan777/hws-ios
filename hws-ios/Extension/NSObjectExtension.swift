dd0d455765c67706c268a408a396987ba04c8f20
Fuyan777 <fuyamiwa713@gmail.com>
2021-04-11 07:58:03 +0900

    add: extensionの追加

---
+++ hws-ios/Extension/NSObjectExtension.swift
@@ -0,0 +1,18 @@
+//
+//  NSObjectExtension.swift
+//  hws-ios
+//
+//  Created by 山田楓也 on 2021/04/11.
+//
+
+import Foundation
+
+extension NSObject {
+    @nonobjc static var className: String {
+        String(describing: self)
+    }
+    
+    var className: String {
+        type(of: self).className
+    }
+}
---
+++ hws-ios/Extension/UITableViewExtension.swift
@@ -0,0 +1,50 @@
+//
+//  UITableViewExtension.swift
+//  hws-ios
+//
+//  Created by 山田楓也 on 2021/04/11.
+//
+
+import UIKit
+
+extension UITableView {
+    func register<T: UITableViewCell>(cellType: T.Type) {
+        let className = cellType.className
+        register(cellType, forCellReuseIdentifier: className)
+    }
+
+    func register<T: UITableViewCell>(cellTypes: [T.Type]) {
+        cellTypes.forEach { register(cellType: $0) }
+    }
+
+    func registerNib<T: UITableViewCell>(cellType: T.Type) {
+        let className = cellType.className
+        let nib = UINib(nibName: className, bundle: nil)
+        register(nib, forCellReuseIdentifier: className)
+    }
+
+    func registerNib<T: UITableViewCell>(cellTypes: [T.Type]) {
+        cellTypes.forEach { registerNib(cellType: $0) }
+    }
+
+    func dequeueReusableCell<T: UITableViewCell>(for indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: T.className, for: indexPath) as? T else {
+            return T()
+        }
+        return cell
+    }
+
+    func dequeueReusableCell<T: UITableViewCell>() -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: T.className) as? T else {
+            return T()
+        }
+        return cell
+    }
+
+    func dequeueReusableView<T: UITableViewHeaderFooterView>() -> T {
+        guard let cell = dequeueReusableHeaderFooterView(withIdentifier: T.className) as? T else {
+            return T()
+        }
+        return cell
+    }
+}
