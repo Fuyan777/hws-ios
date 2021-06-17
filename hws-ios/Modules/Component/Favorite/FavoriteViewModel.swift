2c77e3a3abcef94deb220e20f94d0c6073c69b37
Fuyan777 <fuyamiwa713@gmail.com>
2021-06-17 23:20:07 +0900

    add: お気に入り画面作成

--- hws-ios/Modules/Component/Favorite/FavoriteViewModel.swift
+++ hws-ios/Modules/Component/Favorite/FavoriteViewModel.swift
@@ -5,6 +5,22 @@
 //  Created by 山田楓也 on 2021/04/09.
 //
 
-import Foundation
+import UIKit
 
-class FavoriteViewModel {}
+class FavoriteViewModel: NSObject {}
+
+extension FavoriteViewModel: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(for: indexPath) as SpaceTableViewCell
+        let component = SpaceTableViewCell.Component(
+            imageUrlString: "sss",
+            title: "五稜郭"
+        )
+        cell.setupCell(component: component)
+        return cell
+    }
+}
